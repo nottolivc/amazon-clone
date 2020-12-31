80c13cd10dfc4c846e6924d9a00f6a4ccf88f004
Andrew S <andrews@users-iMac.local>
2020-12-30 23:06:34 -0800

    test auth with default chal firebase, will add custom firebase later + full tweaks

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./components/Checkout";
+import Login from "./Login";
+import Payment from "./components/Payment";
+import Orders from "./components/Orders";
+import { auth } from "./components/firebase";
+import { useStateValue } from "./StateProvider";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51HPvU9DFg5koCdLGJJbNo60QAU99BejacsvnKvT8xnCu1wFLCuQP3WBArscK3RvSQmSIB3N0Pbsc7TtbQiJ1vaOi00X9sIbazL"
+);
+
+function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // will only run once when the component will mount
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("Username: ", authUser);
+
+      if (authUser) {
+        // the user just logged in or was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
