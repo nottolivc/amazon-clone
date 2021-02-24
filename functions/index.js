48777f695a2dc8803aaed19b2b588478e169f261
Andrew Speer <nottolivc@gmail.com>
2021-02-24 14:00:24 -0800

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,10 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-const stripe = require("stripe")(
-  "sk_test_51HPvU9DFg5koCdLGeOEiFvwHat4v8eMjX6SY0YCwxPBQBUPhKy1fPVhiSM5cQtgW7QBG9ydQcXnW57TDxVE2f3H000HSfmEQZF"
-);
+const stripe = require("stripe")("");
+
 
 // API
-
 // - App config
 const app = express();
 
@@ -15,12 +13,12 @@ app.use(cors({ origin: true }));
 app.use(express.json());
 
 // - API routes
-app.get("/", (request, response) => response.status(200).send("hello world"));
+app.get("/", (request, response) => response.status(200).send("API running"));
 
 app.post("/payments/create", async (request, response) => {
   const total = request.query.total;
 
-  console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
+  console.log("Payment Request Recieved for >>> ", total);
 
   const paymentIntent = await stripe.paymentIntents.create({
     amount: total, // subunits of the currency
